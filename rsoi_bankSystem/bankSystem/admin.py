2ab719255ca22ce7644290a75480eb40e982f0ed
Lubov Ilyushshenko <lubastik98@tut.by>
2018-03-09 18:01:39 +0300

    models changes

--- rsoi_bankSystem/bankSystem/admin.py
+++ rsoi_bankSystem/bankSystem/admin.py
@@ -2,3 +2,10 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Customer)
+admin.site.register(models.City)
+admin.site.register(models.Sex)
+admin.site.register(models.Status)
+admin.site.register(models.Citizenship)
+admin.site.register(models.Disability)
+admin.site.register(models.Pensioner)
+admin.site.register(models.Reservist)
